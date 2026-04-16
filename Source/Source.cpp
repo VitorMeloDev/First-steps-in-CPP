479c66f88d1ca7f2496a1c1cf19dd5b56a2c5212
Vitor Melo <89869031+VitorMeloDev@users.noreply.github.com>
2026-04-16 12:09:07 -0300

    Detect collision with Rectangle

--- Source/Source.cpp
+++ Source/Source.cpp
@@ -4,40 +4,59 @@
 #define SCREEN_Y 700
 #define WINDOW_TITLE "Sumo - VM Games"
 
+struct Entity
+{
+	Rectangle rec;
+	Color cor;
+	float speed = 50.f;
+};
+
+void MovePlayer(Entity &player)
+{
+	if (IsKeyDown(KEY_RIGHT))
+	{
+		player.rec.x += player.speed * GetFrameTime();
+	}
+	if (IsKeyDown(KEY_LEFT))
+	{
+		player.rec.x -= player.speed * GetFrameTime();
+	}
+	if (IsKeyDown(KEY_UP))
+	{
+		player.rec.y -= player.speed * GetFrameTime();
+	}
+	if (IsKeyDown(KEY_DOWN))
+	{
+		player.rec.y += player.speed * GetFrameTime();
+	}
+}
+
 int main()
 {
 	InitWindow(SCREEN_X, SCREEN_Y, WINDOW_TITLE);
 	InitAudioDevice();
+	
+	Entity player{ Rectangle{SCREEN_X / 2.f, SCREEN_Y / 2.f, 50.f, 50.f}, BLUE, 300.f };
+	Entity enemy{ Rectangle{SCREEN_X / 3.f, SCREEN_Y / 3.f, 50.f, 50.f}, RED, 0.f };
 
-	float xPos = 0.f;
-	float yPos = 0.f;
-	float speed = 1500.f;
-	int width = 30;
-	int height = 30;
 
 	// main game loop
 	while (!WindowShouldClose())
 	{
 		BeginDrawing();
+		MovePlayer(player);
+		DrawRectangle(player.rec.x , player.rec.y, player.rec.width, player.rec.height, player.cor);
+		DrawRectangle(enemy.rec.x, enemy.rec.y, enemy.rec.width, enemy.rec.height, enemy.cor);
 
-		if (IsKeyDown(KEY_RIGHT))
+		if (CheckCollisionRecs(player.rec, enemy.rec))
 		{
-			xPos += speed * GetFrameTime();
+			player.cor = PURPLE;
 		}
-		if (IsKeyDown(KEY_LEFT))
+		else
 		{
-			xPos -= speed * GetFrameTime();
-		}
-		if (IsKeyDown(KEY_UP))
-		{
-			yPos -= speed * GetFrameTime();
-		}
-		if (IsKeyDown(KEY_DOWN))
-		{
-			yPos += speed * GetFrameTime();
+			player.cor = BLUE;
 		}
 
-		DrawRectangle(xPos, yPos, width, height, WHITE);
 		ClearBackground(BLACK);
 		EndDrawing();
 	}
