25dedc379ce497a5a34ea799259a52f9fcf28a92
Vitor Melo <89869031+VitorMeloDev@users.noreply.github.com>
2026-04-17 11:42:20 -0300

    Create Paddle and CPUPaddle

--- Source/Source.cpp
+++ Source/Source.cpp
@@ -31,7 +31,50 @@ public:
 	}
 };
 
+class Paddle
+{
+public:
+	float x, y;
+	float width, height;
+	float speed;
+
+	void Draw()
+	{
+		DrawRectangle(x, y, width, height, WHITE);
+	}
+
+	void Update()
+	{
+		if (IsKeyDown(KEY_UP) && y > 0)
+		{
+			y -= speed;
+		}
+		if (IsKeyDown(KEY_DOWN) && y + height < GetScreenHeight())
+		{
+			y += speed;
+		}
+	}
+};
+
+class CPUPaddle : public Paddle
+{
+public:
+	void Update(int yBall)
+	{
+		if (y + height/2 > yBall && y > 0)
+		{
+			y -= speed;
+		}
+		if (y + height / 2 < yBall && y + height < GetScreenHeight())
+		{
+			y += speed;
+		}
+	}
+};
+
 Ball ball;
+Paddle player;
+CPUPaddle cpuPaddle;
 
 int main()
 {
@@ -47,20 +90,34 @@ int main()
 	ball.speed_x = 7;
 	ball.speed_y = 7;
 
+	player.speed = 6.f;
+	player.width = 25.f;
+	player.height = 120.f;
+	player.x = 10;
+	player.y = (screenHeight / 2) - (player.height / 2);
+
+	cpuPaddle.speed = 6.f;
+	cpuPaddle.width = 25.f;
+	cpuPaddle.height = 120.f;
+	cpuPaddle.x = screenWidth - cpuPaddle.width - 10;
+	cpuPaddle.y = (screenHeight / 2) - (cpuPaddle.height / 2);
+
 	while (WindowShouldClose() == false)
 	{
 		BeginDrawing();
 
 		// Update
 		ball.Update();
+		player.Update();
+		cpuPaddle.Update(ball.y);
 
 		// Drawing
-		DrawFPS(10, 10);
+		ClearBackground(BLACK);
 		DrawLine(screenWidth/2, 0, screenWidth/2, screenHeight, WHITE);
 		ball.Draw();
-		DrawRectangle(10, screenHeight/2 - 60, 25, 120, WHITE);
-		DrawRectangle(screenWidth - 35, screenHeight / 2 - 60, 25, 120, WHITE);
-		ClearBackground(BLACK);
+		player.Draw();
+		cpuPaddle.Draw();
+		DrawFPS(10, 10);
 
 		EndDrawing();
 	}
